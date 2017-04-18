0d09f0a800a51287232b387bc7ff67b96c691cee
Ted Patrick <ted@vestu.com>
2017-04-18 18:06:08 -0500

    Grid API complete

--- src/iprint.a.col.js
+++ src/iprint.a.col.js
@@ -2,6 +2,10 @@ mul.set( "iprint.a.col" , [] , {
     template: embed( './templates/iprint.a.col.html' ),
     props:{
         "style":String,
+        "e":{
+             type:String,
+             default:''
+        },
         "s":{
              type:String,
              default:''
@@ -25,11 +29,66 @@ mul.set( "iprint.a.col" , [] , {
         "lo":{
              type:String,
              default:''
+        },
+        "sc":{
+             type:String,
+             default:''
+        },
+        "mc":{
+             type:String,
+             default:''
+        },
+        "lc":{
+             type:String,
+             default:''
+        },
+        "su":{
+             type:String,
+             default:''
+        },
+        "mu":{
+             type:String,
+             default:''
+        },
+        "lu":{
+             type:String,
+             default:''
+        },
+        "sp":{
+             type:String,
+             default:''
+        },
+        "mp":{
+             type:String,
+             default:''
+        },
+        "lp":{
+             type:String,
+             default:''
+        },
+        "sl":{
+             type:String,
+             default:''
+        },
+        "ml":{
+             type:String,
+             default:''
+        },
+        "ll":{
+             type:String,
+             default:''
+        },
+        "b":{
+             type:String,
+             default:''
         }
     },
     computed: {
         classObject: function () {
             var d = {};
+            if( this.e ){
+                d[ 'end' ] = true;
+            }
             if( this.s ){
                 d[ 'small-' + this.s ] = true;
             }
@@ -47,6 +106,45 @@ mul.set( "iprint.a.col" , [] , {
             }
             if( this.lo ){
                 d[ 'large-offset' + this.lo ] = true;
+            }
+            if( this.sp ){
+                d[ 'small-push' + this.so ] = true;
+            }
+            if( this.mp ){
+                d[ 'medium-push' + this.mo ] = true;
+            }
+            if( this.lp ){
+                d[ 'large-push' + this.lo ] = true;
+            }
+            if( this.sl ){
+                d[ 'small-pull' + this.so ] = true;
+            }
+            if( this.ml ){
+                d[ 'medium-pull' + this.mo ] = true;
+            }
+            if( this.ll ){
+                d[ 'large-pull' + this.lo ] = true;
+            }
+            if( this.sc ){
+                d[ 'small-centered' ] = true;
+            }
+            if( this.mc ){
+                d[ 'medium-centered' ] = true;
+            }
+            if( this.lc ){
+                d[ 'large-centered' ] = true;
+            }
+            if( this.su ){
+                d[ 'small-uncentered' ] = true;
+            }
+            if( this.mu ){
+                d[ 'medium-uncentered' ] = true;
+            }
+            if( this.lu ){
+                d[ 'large-uncentered' ] = true;
+            }
+            if( this.b ){
+                d[ 'column-block' ] = true;
             }
             return d;
         }
