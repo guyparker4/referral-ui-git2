1bc10fcbfd8e91567e31a0a9dfe681d93f2b6839
Ted Patrick <ted@vestu.com>
2017-05-11 15:47:30 -0500

    close animation for drillsub

--- src/iprint.a.drillsubmenu.js
+++ src/iprint.a.drillsubmenu.js
@@ -2,6 +2,26 @@ mul.set( "iprint.a.drillsubmenu" , [] , {
     template: embed( './templates/iprint.a.drillsubmenu.html' ),
     props:["title","href","active"],
     data(){
-        return { visible:false };
+        return { 
+            visible:false,
+            closing:false,
+            isactive:false
+        };
+    },
+    methods:{
+        close(){
+            this.isactive = false;
+            this.closing = true;
+            var that = this;
+            setTimeout(function(){
+                that.closing = false;
+                that.visible = false;
+            }, 1000);
+
+        },
+        show(){
+            this.isactive = true;
+            this.visible = true;
+        }
     }
 });
