07dbfb0ecab552254cd964c2772dc7d7c14102c6
Ted Patrick <ted@vestu.com>
2017-05-12 10:26:26 -0500

    support for accordion single item open --> mode="1"

--- src/iprint.a.accordionitem.js
+++ src/iprint.a.accordionitem.js
@@ -1,4 +1,17 @@
 mul.set( "iprint.a.accordionitem" , [] , {
     template: embed( './templates/iprint.a.accordionitem.html' ),
-    props:["active","title"]
+    props:["active","title"],
+    methods:{
+        activate(){
+            if( this.active ){
+                this.active=false;
+            }else{
+                this.active=true;
+                console.log( this.$parent );
+                if( this.$parent.childActivate ){
+                    this.$parent.childActivate( this );
+                }
+            }
+        }
+    }
 });
