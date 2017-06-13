4081621936332126fba41e4b6c6090e6e12fbd61
Ted Patrick <ted@vestu.com>
2017-06-13 17:58:13 -0500

    setBillingAddress Complete

--- client/src/brp.o.cart.billing.js
+++ client/src/brp.o.cart.billing.js
@@ -1,9 +1,41 @@
 mul.set( "brp.o.cart.billing" , [] , {
     template: embed( './templates/brp.o.cart.billing.html' ),
     props:["data"],
+    data(){
+        return {
+            billingForm:{
+                firstName: "",
+                lastName: "",
+                companyName: "",
+                street1: "",
+                street2: "",
+                city: "",
+                state: "",
+                zipcode: "",
+                phoneNumber: "",
+                country: "US",
+                isDomestic: true
+            }
+        }
+    },
     methods: {
         onUseAddress () {
             console.log('onUseAddress');
+            brp.services.setBillingAddress( this.billingForm );
+            this.reset();
+        },
+        reset(){
+            this.billingForm.firstName = "";
+            this.billingForm.lastName = "";
+            this.billingForm.companyName = "";
+            this.billingForm.street1 = "";
+            this.billingForm.street2 = "";
+            this.billingForm.city = "";
+            this.billingForm.state = "";
+            this.billingForm.zipcode = "";
+            this.billingForm.phoneNumber = "";
+            this.billingForm.country = "US";
+            this.billingForm.isDomestic = true;
         }
     }
 });
