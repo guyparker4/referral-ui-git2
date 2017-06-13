82e9d6dcaed2f0dd4d6c47e027aaeb7b80d777fe
Ted Patrick <ted@vestu.com>
2017-06-13 15:12:55 -0500

    guest wired to api call

--- client/src/brp.o.checkoutwithoutlogin.js
+++ client/src/brp.o.checkoutwithoutlogin.js
@@ -1,4 +1,19 @@
 mul.set( "brp.o.checkoutwithoutlogin" , [] , {
 	template: embed( './templates/brp.o.checkoutwithoutlogin.html' ),
-	props:[]
+	props:[],
+	data(){
+		return { 
+			guestForm:{
+				login:"",
+				optin:true
+			}
+		};
+	},
+	methods:{
+		guest(){
+			brp.services.guest( this.guestForm.login , this.guestForm.optin );
+			this.guestForm.login = "";
+			this.guestForm.optin = true;
+		}
+	}
 });
